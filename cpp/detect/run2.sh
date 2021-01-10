6ca1e121d06097322993e816baf386784c4f9592
Ivo Stoyanov <ivostoy@gmail.com>
2021-01-10 12:16:24 -0800

    wip

--- cpp/detect/run2.sh
+++ cpp/detect/run2.sh
@@ -2,13 +2,13 @@
 echo cat
 
 ./label_image \
-	--image ../../testdata/lena.bmp \
+	--image ../../testdata/chikadee.bmp \
 	--tflite_model ../../models/mobilenet/mobilenet_v1_1.0_224.tflite \
 	--labels ../../models/mobilenet/mobilenet_v1_1.0_224.labels
 
 
 ./label_image \
-	--image ../../testdata/lena.bmp \
+	--image ../../testdata/bird.bmp \
 	--tflite_model ../../models/mobilenet/mobilenet_v1_1.0_224_quant.tflite \
 	--labels ../../models/mobilenet/mobilenet_v1_1.0_224_quant.labels
 
