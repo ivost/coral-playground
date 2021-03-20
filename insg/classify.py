04875771d1abc6b858ce13006fcf81ef10449e22
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-20 12:45:56 -0700

    detect ok

--- insg/classify.py
+++ insg/classify.py
@@ -1,39 +1,10 @@
-r"""Example using PyCoral to classify a given image using an Edge TPU.
-To run this code, you must attach an Edge TPU attached to the host and
-install the Edge TPU runtime (`libedgetpu.so`) and `tflite_runtime`. For
-device setup instructions, see coral.ai/docs/setup.
-Example usage:
-```
-bash examples/install_requirements.sh
-
--n 2 --verbose 1 --input /home/ivo/Pictures/birds --model  ./models/mobilenet/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite
-
-cd ~/coral
-
-python3 pycoral/examples/classify_image.py \
-  --model test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite  \
-  --labels test_data/inat_bird_labels.txt \
-  --input test_data/images/parrot.jpg
-
-```
-"""
 
 import logging as log
-import os
-import sys
 
-import time
-
-from pathlib import Path
 from pycoral.adapters import classify
 from pycoral.adapters import common
-from pycoral.utils.dataset import read_label_file
-from pycoral.utils.edgetpu import make_interpreter
 
-from common import util
-from common.args import parse_args
 from insg.common import Stats
-
 from insg.engine import Engine
 
 CLASS = "birds"
@@ -42,11 +13,10 @@ version = "v.2021.3.20"
 
 
 class Classify(Engine):
-    # todo cmd line arg to choose ini file
     def __init__(self, log_level=log.INFO):
-        super().__init__("Classification benchmark", version, "classify.ini")
+        super().__init__("Image Classification", version, "classify.ini", log_level)
 
-    def main(self):
+    def run(self):
         stats = Stats()
         repeat = 1 # int(self.c.input.repeat)
         img_proc = self.img_proc
@@ -72,115 +42,6 @@ class Classify(Engine):
 
 
 if __name__ == '__main__':
-    c = Classify(log_level=log.DEBUG)
-    c.main()
-
-
-
-# def main():
-#     t0 = time.perf_counter()
-#     args = init()
-#     log.info(f"Classification benchmark {version}")
-#     # must use raw string and valid regex "cat*.jpg" -> "cat.*\.jpg"
-#     # args.re_path = R'cat.*\.jpg'
-#     # check how many images are available
-#     # log.info(f"{args.input}: {count} images matching {args.re_path}")
-#     # args.input = INPUT_DIR
-#     assert os.path.exists(args.input)
-#     out_dir = None
-#     failed_dir = None
-#     if args.output:
-#         out_dir = Path(args.output, "results").absolute()
-#         if not out_dir.exists():
-#             os.makedirs(out_dir)
-#         failed_dir = Path(out_dir, "not-classified")
-#         if not failed_dir.exists():
-#             os.makedirs(failed_dir)
-#
-#     # by default max files is 10
-#     count = util.count_images(args)
-#     if count < args.count:
-#         args.count = count
-#
-#     log.info(f"Loading {args.count} image(s) from {args.input} with total {count} image(s)")
-#
-#     util.load_images(args)
-#     if len(args.files) == 0:
-#         log.info(f"empty input set")
-#         exit(0)
-#
-#     log.info(f"Loaded {len(args.files)} image(s)")
-#
-#     labels = read_label_file(args.labels) if args.labels else {}
-#
-#     log.info(f"Initializing")
-#     interpreter = make_interpreter(args.model)
-#     interpreter.allocate_tensors()
-#     args.size = common.input_size(interpreter)
-#     log.info(f"Image preparation")
-#     images = util.preproces_images(args)
-#     # Note: The first inference on Edge TPU is slow because it includes
-#     # loading the model into Edge TPU memory.
-#     image = images[0]
-#     common.set_input(interpreter, image)
-#     interpreter.invoke()
-#     classes = classify.get_classes(interpreter, args.top, args.confidence)
-#
-#     repeat = 1
-#     # accumulates inference time
-#     inference_duration = 0
-#     total = 0
-#     idx = 0
-#     failed = 0
-#     log.info(f"START - repeating {repeat} time(s)")
-#     for _ in range(repeat):
-#         for image in images:
-#             path = Path(args.files[idx]).absolute()
-#             total += 1
-#             idx += 1
-#             t1 = time.perf_counter()
-#             # inference
-#             common.set_input(interpreter, image)
-#             interpreter.invoke()
-#             classes = classify.get_classes(interpreter, args.top, args.confidence)
-#             inference_duration += time.perf_counter() - t1
-#             # inference results
-#             count = 0
-#             cid = 0
-#             for c in classes:
-#                 if c.id not in IGNORE_IDS:
-#                     if cid == 0:
-#                         cid = c.id
-#                     if args.verbose > 0:
-#                         log.debug(f"{c.id:4d} - {labels.get(c.id, c.id)}, {c.score:5.2f}")
-#             if cid > 0:
-#                 count += 1
-#                 if out_dir:
-#                     util.copy_to_dir(args, src_file_path=path, dest_dir_path=Path(out_dir, str(cid)))
-#                 continue
-#
-#             failed += 1
-#             if failed_dir:
-#                 util.copy_to_dir(args, src_file_path=path, dest_dir_path=failed_dir)
-#
-#     dur = time.perf_counter() - t0
-#     avg = (inference_duration * 1000) / total
-#     log.info(f"  Total images: {total}, not classified: {failed}")
-#     log.info(f"Inference time: {inference_duration*1000:.0f} ms")
-#     log.info(f"       Average: {avg:.2f} ms")
-#     log.info(f"  Elapsed time: {dur*1000:.0f} ms")
-#     if out_dir:
-#         log.info(f"Results are in {out_dir}")
-#     log.info(f"  END")
-#     return
-#
-#
-# def init():
-#     log.basicConfig(format="[ %(levelname)s ] %(message)s",
-#                     level=log.DEBUG, stream=sys.stdout)
-#     args = parse_args("classification")
-#     return args
-#
-#
-# if __name__ == '__main__':
-#     main()
+    # c = Classify(log_level=log.DEBUG)
+    c = Classify()
+    c.run()
--- insg/detect.py
+++ insg/detect.py
@@ -1,141 +1,86 @@
-# Lint as: python3
-"""
-
--n 100 --verbose 0 --input /test_data/images/coco_val --model ./test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite --labels /test_data/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite
-
-
-"""
-
-from PIL import Image
-from PIL import ImageDraw
-
-from pycoral.adapters import common
-from pycoral.adapters import detect
-from pycoral.utils.dataset import read_label_file
-from pycoral.utils.edgetpu import make_interpreter
 
 import logging as log
-import os
-import sys
-
 import time
 
-from pathlib import Path
+from PIL import ImageDraw
+from pycoral.adapters import common
+from pycoral.adapters import detect
 
-from common import util
-from common.args import parse_args
+from insg.common import Stats
+from insg.engine import Engine
+
+version = "v.2021.3.20"
 
 
-version = "v.2021.3.19"
+class Detect(Engine):
+    def __init__(self, log_level=log.INFO):
+        super().__init__("Image Object Detection", version, "detect.ini", log_level)
 
+    def run(self):
+        log.info(f"Image Object detection {version}")
+        stats = Stats()
+        repeat = int(self.c.input.repeat)
 
-def main():
-    t0 = time.perf_counter()
-    args = init()
-    log.info(f"Object detection benchmark {version}")
+        img_proc = self.img_proc
+        if len(img_proc.files) == 0:
+            log.info(f"empty input set")
+            exit(0)
 
-    assert os.path.exists(args.input)
-    # out_dir = Path(args.input, "..", "results").absolute()
-    # failed_dir = Path(out_dir, "not-recognized")
-    # if not failed_dir.exists():
-    #     os.makedirs(failed_dir)
+        log.info(f"repeating {repeat} time(s)")
+        stats.begin()
 
-    count = util.count_images(args)
-    if count < args.count:
-        args.count = count
+        log.info(f"Initializing")
+        log.info(f"Image preparation, size: {self.size}")
+        images = img_proc.preprocess_images(self.size)
+        log.info(f"{len(images)} images")
 
-    log.info(f"Loading {args.count} image(s) from {args.input} with total {count} image(s)")
-
-    util.load_images(args)
-    if len(args.files) == 0:
-        log.info(f"empty input set")
-        exit(0)
-
-    log.info(f"Loaded {len(args.files)} image(s)")
-
-    labels = read_label_file(args.labels) if args.labels else {}
-
-    log.info(f"Initializing")
-    interpreter = make_interpreter(args.model)
-    interpreter.allocate_tensors()
-    args.size = common.input_size(interpreter)
-    log.info(f"Image preparation, size: {args.size}")
-    images = util.preproces_images(args)
-
-    # Note: The first inference on Edge TPU is slow because it includes
-    # loading the model into Edge TPU memory.
-    image = images[0]
-    scale = (1.0, 1.0)
-    log.debug(f"scale: {scale}, threshold: {args.threshold}")
-
-    common.set_input(interpreter, image)
-    interpreter.invoke()
-    detect.get_objects(interpreter, args.threshold, scale)
-
-    repeat = 1
-    # accumulates inference time
-    inference_duration = 0
-    total = 0
-    idx = 0
-    failed = 0
-    log.info(f"START - repeating {repeat} time(s)")
-    for _ in range(repeat):
-        for image in images:
-            path = Path(args.files[idx]).absolute()
-            total += 1
-            idx += 1
-            t1 = time.perf_counter()
-            # inference
-            common.set_input(interpreter, image)
-            interpreter.invoke()
-            objects = detect.get_objects(interpreter, args.threshold, scale)
-            if not objects:
-                failed += 1
-                if args.verbose > 0:
-                    print(f"No objects detected, image {path}")
-                continue
-            inference_duration += time.perf_counter() - t1
-            # inference results
-            if args.verbose > 1:
-                log.debug(f"detected {len(objects)} objects")
+        image = images[0]
+        scale = (1.0, 1.0)
+        conf = float(self.c.network.confidence)
+        common.set_input(self.coral, image)
+        self.coral.invoke()
+        res = detect.get_objects(self.coral, conf, scale)
+        log.info(f"START - repeating {repeat} time(s)")
+        for _ in range(repeat):
+            failed = 0
+            for idx in range(len(images)):
+                file = self.img_proc.files[idx]
+                image = images[idx]
+                common.set_input(self.coral, image)
+                self.coral.invoke()
+                objects = detect.get_objects(self.coral, conf, scale)
+                if not objects:
+                    failed += 1
+                    continue
+                # inference results
+                log.debug(f"==== in {file} - detected {len(objects)} objects")
                 for obj in objects:
-                    print(labels.get(obj.id, obj.id))
-                    print('  id:    ', obj.id)
-                    print('  score: ', obj.score)
-                    print('  bbox:  ', obj.bbox)
-            # if cid > 0:
-            #     count += 1
-            #     util.copy_to_dir(args, src_file_path=path, dest_dir_path=Path(out_dir, str(cid)))
-            #     continue
+                    label = self.labels.get(obj.id, obj.id)
+                    log.debug(f"{obj.score} {obj.id} {label}")
 
-            # util.copy_to_dir(args, src_file_path=path, dest_dir_path=failed_dir)
+                image = image.convert('RGB')
+                draw_objects(ImageDraw.Draw(image), objects, self.labels)
+                # image.save(args.output)
+                # todo: either move cv2 or use tk
+                image.show()
+                image.close
 
-    dur = time.perf_counter() - t0
-    avg = (inference_duration * 1000) / total
-    log.info(f"  Total images: {total}, nothing detected on {failed}")
-    log.info(f"Inference time: {inference_duration*1000:.0f} ms")
-    log.info(f"       Average: {avg:.2f} ms")
-    log.info(f"  Elapsed time: {dur*1000:.0f} ms")
-    # log.info(f"  END - results are in {out_dir}")
-
-
-def init():
-    log.basicConfig(format="[ %(levelname)s ] %(message)s",
-                    level=log.DEBUG, stream=sys.stdout)
-    args = parse_args("classification")
-    return args
+                # if cid > 0:
+                #     count += 1
+                #     util.copy_to_dir(args, src_file_path=path, dest_dir_path=Path(out_dir, str(cid)))
+                #     continue
+                # util.copy_to_dir(args, src_file_path=path, dest_dir_path=failed_dir)
 
 
 def draw_objects(draw, objs, labels):
     """Draws the bounding box and label for each object."""
     for obj in objs:
+        label = labels.get(obj.id, obj.id)
         bbox = obj.bbox
-        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
-                       outline='red')
-        draw.text((bbox.xmin + 10, bbox.ymin + 10),
-                  '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
-                  fill='red')
+        draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)], outline='yellow')
+        draw.text((bbox.xmin + 10, bbox.ymin + 10), f"{obj.score} - {label}", fill='yellow')
 
 
 if __name__ == '__main__':
-    main()
+    d = Detect(log_level=log.DEBUG)
+    d.run()
