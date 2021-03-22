a8142228e977015cff23e3352f6989cb0cd2ec38
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-22 14:04:22 -0700

    yolov4

--- yolov4-tflite/objdet.py
+++ yolov4-tflite/objdet.py
@@ -1,3 +1,7 @@
+"""
+https://wiki.loliot.net/docs/lang/python/libraries/yolov4/python-yolov4-edge-tpu/
+
+"""
 from yolov4.tflite import YOLOv4
 
 yolo = YOLOv4()
@@ -7,9 +11,20 @@ yolo.config.parse_cfg("./config/yolov4-tiny-relu-tpu.cfg")
 # yolo.summary()
 yolo.load_tflite("./config/yolov4-tiny-int8_edgetpu.tflite")
 
-yolo.inference("./data/kite.jpg")
+video = "/home/ivo/Videos/airport-02.m4v"
 
-yolo.inference("./data/kite.jpg")
+#video = "/home/ivo/Videos/airport-03.mp4"
 
-yolo.inference("./data/kite.jpg")
+#video = "../videos/RTSP/rtsp0.mkv"
+#video = "../videos/airport-03-HD.mp4"
+
+# size = (1920, 1080)
+# size = (192, 108)
+
+# yolo.inference(video, is_image=False, cv_frame_size=size)
+conf = 0.2
+yolo.inference(video, is_image=False, prob_thresh=conf)
+
+# yolo.inference("./data/kite.jpg")
+# yolo.inference("./data/kite.jpg")
 
