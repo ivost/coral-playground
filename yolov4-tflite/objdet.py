6ad5356a4b2f2769cc74160c7c9bf4743371118c
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-22 13:05:19 -0700

    yolov4

---
+++ yolov4-tflite/objdet.py
@@ -0,0 +1,15 @@
+from yolov4.tflite import YOLOv4
+
+yolo = YOLOv4()
+
+yolo.config.parse_names("./dataset/coco.names")
+yolo.config.parse_cfg("./config/yolov4-tiny-relu-tpu.cfg")
+# yolo.summary()
+yolo.load_tflite("./config/yolov4-tiny-int8_edgetpu.tflite")
+
+yolo.inference("./data/kite.jpg")
+
+yolo.inference("./data/kite.jpg")
+
+yolo.inference("./data/kite.jpg")
+
