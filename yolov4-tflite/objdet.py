5588f1e41daced8610fa3bc3f8bad8b0ca2f098e
Ivo Stoyanov <ivostoy@gmail.com>
2021-03-22 15:31:28 -0700

    yolov4

--- yolov4-tflite/objdet.py
+++ yolov4-tflite/objdet.py
@@ -12,25 +12,26 @@ yolo.config.parse_cfg("./config/yolov4-tiny-relu-tpu.cfg")
 # yolo.summary()
 yolo.load_tflite("./config/yolov4-tiny-int8_edgetpu.tflite")
 
-video = "/home/ivo/Videos/airport-01.m4v"
+name = "airport-03"
+video = "/home/ivo/Videos/" + name + ".m4v"
 
 #video = "/home/ivo/Videos/airport-03.mp4"
-
 #video = "../videos/RTSP/rtsp0.mkv"
 #video = "../videos/airport-03-HD.mp4"
 
 size = (1920, 1080)
-# size = (192, 108)
+# fourcc = cv2.VideoWriter_fourcc(*'MPEG')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-outp = "/home/ivo/video_out/a.avi"
+fps = 24.0
 
-fourcc = cv2.VideoWriter_fourcc(*'MPEG')
-vw = cv2.VideoWriter(outp, fourcc, 20.0, size)
+out = video.replace(".m4v", "-yolov4-edgetpu.avi")
 
-# yolo.inference(video, is_image=False, cv_frame_size=size)
+print(f"input: {video}, output: {out}")
+vw = cv2.VideoWriter(out, fourcc, fps, size)
+
 conf = 0.2
 yolo.inference(video, is_image=False, prob_thresh=conf, video_writer=vw)
-
 vw.release()
 
 # yolo.inference("./data/kite.jpg")
